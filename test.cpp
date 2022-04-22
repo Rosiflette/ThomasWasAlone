3b193c359f419700ed0911944de8d96503a7a14a
Audrey portable <diep.audrey@gmail.com>
2022-04-22 10:24:03 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,24 @@
+class Level{
+    QuadTree Rectangle obstacles[];
+    read level;
+}
+
+class QuadTree{
+    childs
+}
+
+class Rectangle{
+    int x, y;
+    int length;
+    int height;
+    color;
+}
+
+class Joueur{
+    Rectangle joueur;
+    collision();
+    movement();
+    sauter();
+}
+
+Joueurs = [joueur1, joueur2..] 
